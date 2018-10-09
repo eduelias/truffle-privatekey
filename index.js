0d46f21a5262c5e10442a949b8893f0097c26688
Eduardo Elias Saleh <eduardo@lab15.io>
2018-10-09 11:40:15 +0200

    Inital commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const ProviderEngine = require("web3-provider-engine");
+const FiltersSubprovider = require('web3-provider-engine/subproviders/filters.js');
+const WalletSubprovider = require('web3-provider-engine/subproviders/wallet.js');
+const ProviderSubprovider = require("web3-provider-engine/subproviders/provider.js");
+const EthereumjsWallet = require('ethereumjs-wallet');
+const Web3 = require("web3");
+const NonceSubprovider = require('web3-provider-engine/subproviders/nonce-tracker.js');
+
+function PrivateKeyProvider(providerUrl) {
+    this.engine = new ProviderEngine();
+
+    this.engine.addProvider(new FiltersSubprovider());
+    this.engine.addProvider(new NonceSubprovider());
+
+    // HACK: `sendAsync` was removed
+    if (!Web3.providers.HttpProvider.prototype.sendAsync) {
+        Web3.providers.HttpProvider.prototype.sendAsync = Web3.providers.HttpProvider.prototype.send
+    }
+
+    this.engine.addProvider(new ProviderSubprovider(new Web3.providers.HttpProvider(providerUrl)));
+    this.engine.start();
+}
+
+PrivateKeyProvider.prototype.sendAsync = function() {
+    //console.log('Engine: ', this.engine);
+    if (arguments[0].params && arguments[0].params.length > 0 && arguments[0].params[0].fromPvk) {
+
+        const privateKey = arguments[0].params[0].fromPvk;
+        this.wallet = EthereumjsWallet.fromPrivateKey(new Buffer(privateKey, "hex"));
+        this.address = "0x" + this.wallet.getAddress().toString("hex");
+
+        this.engine.addProvider(new WalletSubprovider(this.wallet, {}));
+        console.log('Arguments: ', arguments[0].params[0].fromPvk);
+    }
+    this.engine.sendAsync.apply(this.engine, arguments);
+};
+
+PrivateKeyProvider.prototype.send = function() {
+    //console.log('Engine: ', this.engine);
+    if (arguments[0].params && arguments[0].params.length > 0 && arguments[0].params[0].fromPvk) {
+
+        const privateKey = arguments[0].params[0].fromPvk;
+        this.wallet = EthereumjsWallet.fromPrivateKey(new Buffer(privateKey, "hex"));
+        this.address = "0x" + this.wallet.getAddress().toString("hex");
+
+        this.engine.addProvider(new WalletSubprovider(this.wallet, {}));
+        console.log('Arguments: ', arguments[0].params[0].fromPvk);
+    }
+    return this.engine.send.apply(this.engine, arguments);
+};
+
+
+module.exports = PrivateKeyProvider;
